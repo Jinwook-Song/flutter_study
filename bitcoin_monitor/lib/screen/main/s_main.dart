e51b925b23a697b0ee8ee6006b65d9845459dcf7
jinwook song <wlsdnr129@naver.com>
2024-04-21 18:50:06 +0900

    feat(websocket): bitcoin monitor

--- bitcoin_monitor/lib/screen/main/s_main.dart
+++ bitcoin_monitor/lib/screen/main/s_main.dart
@@ -1,4 +1,10 @@
+import 'dart:convert';
+
+import 'package:fast_app_base/common/dart/extension/num_duration_extension.dart';
+import 'package:fast_app_base/common/widget/animated_number_text.dart';
+import 'package:fast_app_base/common/widget/line_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:web_socket_channel/io.dart';
 
 import '../../common/common.dart';
 import 'w_menu_drawer.dart';
@@ -11,15 +17,56 @@ class MainScreen extends StatefulWidget {
 }
 
 class MainScreenState extends State<MainScreen> {
+  final wsUrl = Uri.parse('wss://stream.binance.com:9443/ws/btcusdt@trade');
+  late final channel = IOWebSocketChannel.connect(wsUrl);
+  late final Stream<dynamic> stream;
+
+  String priceString = 'Loading';
+  final List<double> priceList = [];
+
+  final _durationInterval = 1.seconds;
+  DateTime lastUpdatedTime = DateTime.now();
+
+  @override
+  void initState() {
+    super.initState();
+    stream = channel.stream;
+    stream.listen((event) {
+      final obj = json.decode(event);
+      final double price = double.parse(obj['p']);
+      if (DateTime.now().difference(lastUpdatedTime) > _durationInterval) {
+        priceList.add(price);
+        priceString = price.toDoubleStringAsFixed();
+        lastUpdatedTime = DateTime.now();
+        setState(() {});
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: const MenuDrawer(),
-      body: Container(
-        color: context.appColors.seedColor.getMaterialColorValues[200],
-        child: const SafeArea(
-          child: Placeholder(),
-        ),
+      body: Stack(
+        children: [
+          SafeArea(
+              child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                AnimatedNumberText(
+                  priceString,
+                  textStyle: const TextStyle(
+                    fontSize: 50,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  duration: 50.ms,
+                ),
+                LineChartWidget(priceList, maxPrice: 64950)
+              ],
+            ),
+          )),
+        ],
       ),
     );
   }
