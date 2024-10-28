c729ae7e0c57decc5f34d3366546990903fa2c4e
jinwook song <wlsdnr129@naver.com>
2024-10-29 00:10:20 +0900

    init: clean architecture starter

---
+++ clean_architecture_starter/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:clean_architecture_starter/model.dart';
+import 'package:clean_architecture_starter/view_model.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'view.dart';
+
+void main() {
+  runApp(const CounterApp());
+}
+
+class CounterApp extends StatefulWidget {
+  const CounterApp({super.key});
+
+  @override
+  State<CounterApp> createState() => _CounterAppState();
+}
+
+class _CounterAppState extends State<CounterApp> {
+  @override
+  void initState() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: CounterView(
+        counterViewModel: CounterViewModel(CounterModel()),
+        counterModeViewModel: CounterModeViewModel(CouterModeModel()),
+      ),
+    );
+  }
+}
---
+++ clean_architecture_starter/lib/view.dart
@@ -0,0 +1,79 @@
+import 'package:clean_architecture_starter/model.dart';
+import 'package:clean_architecture_starter/view_model.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+// TODO: MVC 패턴을 MVVM 패턴으로 변경하여 카운터 앱이 동작하도록 변경하세요.
+
+class CounterView extends StatelessWidget {
+  final CounterViewModel counterViewModel;
+  final CounterModeViewModel counterModeViewModel;
+  const CounterView({
+    super.key,
+    required this.counterViewModel,
+    required this.counterModeViewModel,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('MVVM'),
+        actions: [
+          IconButton(
+            onPressed: onChangedMode,
+            icon: const Icon(CupertinoIcons.arrow_2_squarepath),
+          )
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            ValueListenableBuilder(
+              valueListenable: counterViewModel.counter,
+              builder: (context, value, child) => Text(
+                value.toString(),
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: execute,
+        child: ValueListenableBuilder(
+            valueListenable: counterModeViewModel.counterMode,
+            builder: (context, value, child) => Icon(value.icon)),
+      ),
+    );
+  }
+
+  void onChangedMode() {
+    counterModeViewModel.toggleMode();
+  }
+
+  void execute() {
+    switch (counterModeViewModel.counterMode.value) {
+      case CounterMode.plus:
+        counterViewModel.increment();
+      case CounterMode.minus:
+        counterViewModel.decrement();
+    }
+  }
+}
+
+extension on CounterMode {
+  IconData get icon {
+    switch (this) {
+      case CounterMode.plus:
+        return CupertinoIcons.add;
+      case CounterMode.minus:
+        return CupertinoIcons.minus;
+    }
+  }
+}
