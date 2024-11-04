35fb58e549cd536cf59b46a66cdb85b9225d8575
jinwook song <wlsdnr129@naver.com>
2024-11-04 12:07:54 +0900

    clind(server): clind rest api

--- clind/app/clind/lib/screen.dart
+++ clind/app/clind/lib/screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:tool_clind_component/component.dart';
+import 'package:tool_clind_theme/gen/gen.dart';
 import 'package:tool_clind_theme/theme.dart';
+import 'package:tool_clind_network/network.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -13,33 +15,28 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   void initState() {
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
     super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+    WidgetsBinding.instance.addPostFrameCallback(
+      (_) async {
+        final api = await ClindRestClient().get('/post-api/channels');
+        print(api.data);
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: context.colorScheme.black,
       appBar: ClindAppBar(
         context: context,
-        title: ClindSearchTextField(onSearch: (String text) {}),
+        title: ClindAppBarTitle(
+            title: Text(
+          'Clind',
+          style: context.textTheme.poppins18Bold
+              .copyWith(color: ColorName.mainRed),
+        )),
       ),
-      body: Column(
-        children: [
-          ClindSearchBar(text: '', onTap: () {}),
-          const ClindLoadingSearchBar(),
-          ClindSortFilter(
-            text: '최신순',
-            onTap: () {},
-          )
-        ],
-      ),
-      floatingActionButton: ClindWriteButton(onTap: () {
-        ClindDialog.showConfirm(context, title: 'test', onConfirm: () {});
-      }),
-      bottomNavigationBar:
-          ClindChatBottomNavigationBar(onSend: (String text) {}),
     );
   }
 }
