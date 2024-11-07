d4dba559340b16efc89e430b47234dcb6770debe
jinwook song <wlsdnr129@naver.com>
2024-11-07 21:07:47 +0900

    di: write bloc provider

---
+++ clind/app/clind/di/lib/src/bloc/write.dart
@@ -0,0 +1,66 @@
+import 'package:core_flutter_bloc/flutter_bloc.dart';
+import 'package:data/data.dart';
+import 'package:domain/domain.dart';
+import 'package:flutter/material.dart';
+import 'package:presentation/presentation.dart';
+import 'package:tool_clind_network/network.dart';
+
+class WriteBlocProvider extends StatelessWidget {
+  final Widget child;
+  const WriteBlocProvider({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return FlowMultiBlocProvider(
+      providers: [
+        FlowRepositoryProvider<IPostRemoteDataSource>(
+          create: (context) => PostRemoteDataSource(PostApi(ClindRestClient())),
+        ),
+        FlowRepositoryProvider<IProfileRemoteDataSource>(
+          create: (context) =>
+              ProfileRemoteDataSource(ProfileApi(ClindRestClient())),
+        ),
+      ],
+      child: FlowRepositoryProvider<WriteDataSource>(
+        create: (context) => WriteDataSource(
+          context.readFlowRepository<IPostRemoteDataSource>(),
+          context.readFlowRepository<IProfileRemoteDataSource>(),
+        ),
+        child: FlowRepositoryProvider<IWriteRepository>(
+          create: (context) => WriteRepository(
+            context.readFlowRepository<WriteDataSource>(),
+          ),
+          child: FlowMultiRepositoryProvider(
+            providers: [
+              FlowRepositoryProvider<CreatePostUseCasae>(
+                create: (context) => CreatePostUseCasae(
+                  context.readFlowRepository<IWriteRepository>(),
+                ),
+              ),
+              FlowRepositoryProvider<GetMeUseCasae>(
+                create: (context) => GetMeUseCasae(
+                  context.readFlowRepository<IWriteRepository>(),
+                ),
+              ),
+            ],
+            child: FlowMultiBlocProvider(
+              providers: [
+                FlowBlocProvider<WritePostCubit>(
+                  create: (context) => WritePostCubit(
+                    context.readFlowRepository<CreatePostUseCasae>(),
+                  ),
+                ),
+                FlowBlocProvider<WriteMeCubit>(
+                  create: (context) => WriteMeCubit(
+                    context.readFlowRepository<GetMeUseCasae>(),
+                  ),
+                ),
+              ],
+              child: child,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
