0ef68395a2e755bfe9354abba66672e3f4283f4e
jinwook song <wlsdnr129@naver.com>
2024-11-22 22:33:52 +0900

    refactor: core

--- wabiz/client/wabiz/lib/main.dart
+++ wabiz/client/wabiz/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:wabiz/core/theme.dart';
+import 'package:wabiz/core/core.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,10 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      theme: wabizDefaultTheme,
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
