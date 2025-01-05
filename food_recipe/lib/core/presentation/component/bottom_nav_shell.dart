bc5290f9949de7cc38046d31ba2f15b010904f12
jinwook song <wlsdnr129@naver.com>
2025-01-05 14:11:04 +0900

    feat(nav bar): custom nav bar

--- food_recipe/lib/core/presentation/component/bottom_nav_shell.dart
+++ food_recipe/lib/core/presentation/component/bottom_nav_shell.dart
@@ -3,6 +3,8 @@ import 'package:food_recipe/core/core.dart';
 import 'package:food_recipe/ui/ui.dart';
 
 class BottomNavShell extends StatelessWidget {
+  static const double bottomNavBarHeight = 80;
+
   final StatefulNavigationShell navigationShell;
   final int currentIndex;
   final void Function(int index) onChangeIndex;
@@ -16,119 +18,174 @@ class BottomNavShell extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const double bottomNavBarHeight = 80;
-    const double bottomPadding = 20;
-
-    return Stack(
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(
-            bottom: bottomNavBarHeight + bottomPadding,
-          ),
-          child: navigationShell,
-        ),
-        Positioned(
-          left: 0,
-          right: 0,
-          bottom: 0,
-          child: Theme(
-            data: Theme.of(context).copyWith(
-              splashColor: Colors.transparent,
-              highlightColor: Colors.transparent,
+    return Material(
+      child: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(
+              bottom: bottomNavBarHeight,
             ),
-            child: SafeArea(
-              bottom: false,
-              child: NavigationBar(
-                elevation: 1,
-                height: bottomNavBarHeight,
-                surfaceTintColor: AppColors.gray2,
-                labelBehavior: NavigationDestinationLabelBehavior.alwaysHide,
-                indicatorColor: Colors.transparent,
-                onDestinationSelected: navigationShell.goBranch,
-                selectedIndex: currentIndex,
-                destinations: [
-                  NavigationDestination(
-                    icon: const Icon(
-                      Icons.home_outlined,
-                      color: AppColors.gray4,
-                    ),
-                    selectedIcon: const Stack(
-                      children: [
-                        Icon(
-                          Icons.home,
-                          color: AppColors.primary40,
-                        ),
-                        Icon(
-                          Icons.home_outlined,
-                          color: AppColors.primary100,
-                        ),
-                      ],
-                    ),
-                    label: Routes.home.name,
-                  ),
-                  NavigationDestination(
-                    icon: const Icon(
-                      Icons.bookmark_border,
-                      color: AppColors.gray4,
-                    ),
-                    selectedIcon: const Stack(
-                      children: [
-                        Icon(
-                          Icons.bookmark,
-                          color: AppColors.primary40,
-                        ),
-                        Icon(
-                          Icons.bookmark_border,
-                          color: AppColors.primary100,
-                        ),
-                      ],
-                    ),
-                    label: Routes.savedRecipes.name,
-                  ),
-                  NavigationDestination(
-                    icon: const Icon(
-                      Icons.notifications_outlined,
-                      color: AppColors.gray4,
-                    ),
-                    selectedIcon: const Stack(
-                      children: [
-                        Icon(
-                          Icons.notifications,
-                          color: AppColors.primary40,
-                        ),
-                        Icon(
-                          Icons.notifications_outlined,
-                          color: AppColors.primary100,
-                        ),
-                      ],
-                    ),
-                    label: Routes.notifications.name,
-                  ),
-                  NavigationDestination(
-                    icon: const Icon(
-                      Icons.person_outline,
-                      color: AppColors.gray4,
-                    ),
-                    selectedIcon: const Stack(
-                      children: [
-                        Icon(
-                          Icons.person,
-                          color: AppColors.primary40,
-                        ),
-                        Icon(
-                          Icons.person_outline,
-                          color: AppColors.primary100,
-                        ),
-                      ],
-                    ),
-                    label: Routes.profile.name,
-                  ),
-                ],
-              ),
+            child: navigationShell,
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: CustomNavBar(
+              selectedIndex: currentIndex,
+              onNavTap: navigationShell.goBranch,
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
+}
+
+class CustomNavBar extends StatelessWidget {
+  final int selectedIndex;
+  final void Function(int) onNavTap;
+  const CustomNavBar({
+    super.key,
+    required this.selectedIndex,
+    required this.onNavTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Container(
+        height: BottomNavShell.bottomNavBarHeight,
+        color: AppColors.white,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            for (final item in NavItem.values)
+              Expanded(
+                child: GestureDetector(
+                  onTap: () => onNavTap(item.tabIndex),
+                  behavior: HitTestBehavior.opaque,
+                  child: NavDestination(
+                    icon: item.icon,
+                    selectedIcon: item.selectedIcon,
+                    isSelected: selectedIndex == item.tabIndex,
+                  ),
+                ),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class NavDestination extends StatelessWidget {
+  final Widget icon;
+  final Widget? selectedIcon;
+  final bool isSelected;
+  const NavDestination({
+    super.key,
+    required this.icon,
+    this.selectedIcon,
+    required this.isSelected,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    if (isSelected) return selectedIcon ?? icon;
+    return icon;
+  }
+}
+
+enum NavItem {
+  home(
+    tabIndex: 0,
+    icon: Icon(
+      Icons.home_outlined,
+      color: AppColors.gray4,
+    ),
+    selectedIcon: Stack(
+      fit: StackFit.expand,
+      children: [
+        Icon(
+          Icons.home,
+          color: AppColors.primary40,
+        ),
+        Icon(
+          Icons.home_outlined,
+          color: AppColors.primary100,
+        ),
+      ],
+    ),
+  ),
+  bookmark(
+    tabIndex: 1,
+    icon: Icon(
+      Icons.bookmark_outline,
+      color: AppColors.gray4,
+    ),
+    selectedIcon: Stack(
+      fit: StackFit.expand,
+      children: [
+        Icon(
+          Icons.bookmark,
+          color: AppColors.primary40,
+        ),
+        Icon(
+          Icons.bookmark_outline,
+          color: AppColors.primary100,
+        ),
+      ],
+    ),
+  ),
+  notifications(
+    tabIndex: 2,
+    icon: Icon(
+      Icons.notifications_outlined,
+      color: AppColors.gray4,
+    ),
+    selectedIcon: Stack(
+      fit: StackFit.expand,
+      children: [
+        Icon(
+          Icons.notifications,
+          color: AppColors.primary40,
+        ),
+        Icon(
+          Icons.notifications_outlined,
+          color: AppColors.primary100,
+        ),
+      ],
+    ),
+  ),
+  profile(
+    tabIndex: 3,
+    icon: Icon(
+      Icons.person_outline,
+      color: AppColors.gray4,
+    ),
+    selectedIcon: Stack(
+      fit: StackFit.expand,
+      children: [
+        Icon(
+          Icons.person,
+          color: AppColors.primary40,
+        ),
+        Icon(
+          Icons.person_outline,
+          color: AppColors.primary100,
+        ),
+      ],
+    ),
+  );
+
+  final int tabIndex;
+  final Widget icon;
+  final Widget? selectedIcon;
+
+  const NavItem({
+    required this.tabIndex,
+    required this.icon,
+    required this.selectedIcon,
+  });
 }
